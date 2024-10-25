12e1a7fbb3c8ef344935d11c5cac2f8ed672db79
Aman Singh <amansa6@gmail.com>
2024-10-25 23:54:01 +0200

    Initial commit for React and Tailwind Setup

---
+++ client/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}", // Ensure you include all relevant file types
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
